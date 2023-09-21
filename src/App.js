7de1a5414f884004a71b99d193648f7b336e0638
seonyeonghun <sprax@naver.com>
2023-09-21 09:24:21 +0900

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState("");
+  const onChange = (e) => setMoney(e.target.value);  
+  
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setMoney("");
+  }
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then(response => response.json())
+    .then(json => {
+      //console.log(json);
+      setCoins(json);
+      setLoading(false);
+    });
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className='App'>
+        <h1>the coins! (total: {loading ? 0 : coins.length})</h1>
+        {loading ? <strong>loading...</strong> : null}
+        <form onSubmit={onSubmit}>
+          <input type="number" value={money} onChange={onChange} placeholder="how much you have by KRW?" />
+          <button>change USD to KRW</button>
+        </form>
+        <ul>
+          {coins.map(coin => <li key={coin.id}>{coin.name} ({coin.symbol}) : ${money ? coin.quotes.USD.price : coin.quotes.USD.price / money}</li>)}  
+        </ul>
+      </div>
+    </Fragment>
   );
 }
 
